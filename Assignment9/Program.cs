c789cf0994b7abfb9ee80b57d06490ff6f83c58b
mariam-hamada <168047165+mariam-hamada@users.noreply.github.com>
2026-08-24 23:05:07 +0300

    Part o1 - Theoretical Questions -->Question 1

--- Assignment9/Program.cs
+++ Assignment9/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Assignment9
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Part 01 — Theoretical Questions
+
+            #region Question1
+
+            //a) What happens when you assign one object variable to another object variable?
+            //The reference (address) is copied, not the actual object.
+            //b) Does assigning one object to another create a new object? Explain.
+            //No, Assigning one object variable to another does not create a new object. This only copies the reference to the existing object.
+            //c) What is the difference between copying an object and copying its reference ?
+            //==> Copying the object: A new, separate object is created with the same data as the original.Changes to one object do not affect the other.
+            //==> Copying the reference: Both variables point to the same object. Changing the object through one variable affects the other.
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
